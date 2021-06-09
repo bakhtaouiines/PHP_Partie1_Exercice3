70d40bfcdbe44b3ae55855e4f7820fbcb5c3f682
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 14:53:16 +0200

    correction ajout HTML

--- exercice1.3.php
+++ exercice1.3.php
@@ -1,8 +1,22 @@
-<?php
-$km = 1;
-echo $km. '<br>';
-$km = 3;
-echo $km. '<br>';
-$km = 125;
-echo $km. '<br>';
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1.3</title>
+</head>
+<body>
+    <h1>Exercice 3 Partie 1 : Les variables PHP</h1>
+    <p>
+        <?php
+            $km = 1;
+            echo nl2br ($km."\n");
+            $km = 3;
+            echo nl2br ($km."\n");
+            $km = 125;
+            echo $km;
+        ?>
+    </p>
+</body>
+</html>
